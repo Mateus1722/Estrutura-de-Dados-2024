5082149448feaca3110ef1265c419ad7a538117f
Mateus1722 <madealopes123@gmail.com>
2024-09-11 22:04:08 -0300

    Pilhas

---
+++ Atividades/Exercicios Aulas/ExercicioPilhas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PalindromoComPilha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma palavra: ");
+            string palavra = Console.ReadLine().ToLower(); // Convertemos para minúsculas para ignorar maiúsculas e minúsculas
+
+            // Criamos uma pilha de caracteres
+            Stack<char> pilha = new Stack<char>();
+
+            // Empilhamos cada caractere da palavra
+            foreach (char caractere in palavra)
+            {
+                pilha.Push(caractere); // Adiciona um caractere ao topo da pilha
+            }
+
+            // Comparamos cada caractere da palavra com o topo da pilha
+            bool ehPalindromo = true;
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                if (palavra[i] != pilha.Pop()) // Remove e retorna o elemento do topo da pilha
+                {
+                    ehPalindromo = false;
+                    break;
+                }
+            }
+
+            if (ehPalindromo)
+            {
+                Console.WriteLine("A palavra é um palíndromo!");
+            }
+            else
+            {
+                Console.WriteLine("A palavra não é um palíndromo.");
+            }
+        }
+    }
+}
---
+++ Atividades/Pilhas/Program.cs
@@ -0,0 +1,20 @@
+﻿//Basicamente as pilhas trabalham com uma estrutura LIFO -> Last In First Out
+//Três metodos importantes:
+//Push() -> Insere um elemento no topo da pilha 
+//Pop() -> remove um elemento do topo e o retorna
+//Peek() -> Retorna o elemento do topo sem removê-lo
+
+// Invertendo as bolas 
+Stack<char> caracteres = new Stack<char>();
+foreach(char c in "DARCY DA MONTANHA")
+{
+    caracteres.Push(c);
+}
+
+//Desempilhando
+string invertido = string.Empty;
+while(caracteres.Count > 0)
+{
+    invertido += caracteres.Pop();
+}
+Console.WriteLine(invertido);
