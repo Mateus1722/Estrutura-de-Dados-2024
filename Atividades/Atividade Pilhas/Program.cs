9c44763760fa17178d3f7d8c5ee55154733d0947
Mateus1722 <madealopes123@gmail.com>
2024-09-18 22:08:49 -0300

    Atualização Pilhas e Filas

---
+++ Atividades/Atividade Pilhas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SistemaDeSenhas
+{
+    private Stack<int> filaDeSenhas = new Stack<int>(); // Nossa fila de senhas
+    private int proximaSenha = 100; // Número da próxima senha
+
+    public void NovaSenha()
+    {
+        filaDeSenhas.Push(proximaSenha); // Adiciona uma nova senha na fila
+        Console.WriteLine($"Sua senha é: {proximaSenha}");
+        proximaSenha+=100; // Prepara a próxima senha
+    }
+
+    public void AtenderProximo()
+    {
+        if (filaDeSenhas.Count == 0)
+        {
+            Console.WriteLine("Não há mais ninguém na fila.");
+        }
+        else
+        {
+            Console.WriteLine($"Próximo: {filaDeSenhas.Pop()}");
+        }
+    }
+
+    static void Main()
+    {
+        SistemaDeSenhas sistema = new SistemaDeSenhas();
+
+        // Simulando 5 pessoas chegando e sendo atendidas
+        for (int i = 0; i < 10; i++)
+        {
+            sistema.NovaSenha();
+            sistema.AtenderProximo();
+        }
+    }
+}
---
+++ Filas/Program.cs
@@ -0,0 +1,36 @@
+﻿// Uma fila é a estrutura de dados que pode ser aplicada para representar a fila de atendimento da cantina 
+// As pessoas que chegam entram no final da fila e as primeiras que chegram são as primeiras a serem atendidas.
+
+// A função ENQUEUE é reponsavel por adicionar novos elementos a fila 
+
+// A função DEQUEUE remove elementos da fila.
+
+// Desta forma obedece o principio do FIFO (First In, First Out -> Primeiro que entra é o priemro que sai)
+
+using Filas;
+
+Random random = new Random();
+
+CallCenter center = new();
+center.Call(1234);
+center.Call(1369);
+center.Call(2468);
+center.Call(1478);
+
+while(center.AreWaitingCalls())
+{
+    Thread.Sleep(3000);
+    IncomingCall call = center.Answer("Mateus");
+    Console.WriteLine( 
+       @$"{DateTime.Now:HH:mm:ss} 
+           Chamado: #{call.Id}
+           De: {call.ClientId}
+           Atendido por: {call.Consultant}"
+    );
+    Thread.Sleep(random.Next(1000, 10000));
+    center.End(call);
+    Console.WriteLine(
+       @$"Chamado: {call.Id}
+          Encerrado ás: {call.EndTime}"
+    );
+}
