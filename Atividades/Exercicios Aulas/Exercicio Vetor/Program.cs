90651da62f6b4ad55381a6f7567c8e22ef639cbb
Mateus1722 <madealopes123@gmail.com>
2024-09-28 18:47:54 -0300

    Exercicio Vetor

---
+++ Atividades/Exercicios Aulas/Exercicio Vetor/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definindo os vetores para armazenar os números
+        int[] pares = new int[100];
+        int[] impares = new int[100];
+
+        // Preenchendo os vetores
+        for (int i = 0; i < 100; i++)
+        {
+            pares[i] = i * 2;      // Números pares
+            impares[i] = i * 2 + 1; // Números ímpares
+        }
+
+        // Imprimindo os números pares
+        Console.WriteLine("Números Pares:");
+        foreach (int num in pares)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+
+        // Imprimindo os números ímpares
+        Console.WriteLine("Números Ímpares:");
+        foreach (int num in impares)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
