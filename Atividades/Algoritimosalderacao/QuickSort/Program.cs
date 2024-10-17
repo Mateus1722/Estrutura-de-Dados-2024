a611c46e53a7a0bbc7f10273b71cd75d2209e660
Mateus1722 <madealopes123@gmail.com>
2024-10-16 21:40:48 -0300

    QuickSort

---
+++ Atividades/Algoritimosalderacao/QuickSort/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Windows.Markup;
+
+Console.WriteLine("Testando o Quick Sort");
+
+int[] values = new int [] {0 , 15, 24, 1, 3, 6, 12, 9};
+
+QuickSort.QuickSort.Print<int>(values);
+Console.WriteLine("Iniciando ordenação.");
+QuickSort.QuickSort.Print<int>(values);
+QuickSort.QuickSort.Sort(values, 0, values.Length - 1);
+Console.WriteLine("Finalizando ordenação");
+QuickSort.QuickSort.Print<int>(values);
